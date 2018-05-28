7ce70fb2a69f8cc844760e83e298ee7190c52631
joel <jamorran@latrobe.students.edu.au>
2018-05-28 17:58:36 +1000

    cors added

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const http = require('http');
 const path = require('path');
-
+const cors = require('cors');
 
 
 const app = express();
+
+app.use(cors());
 
 app.use(express.static(path.join(__dirname, 'dist')));
 
