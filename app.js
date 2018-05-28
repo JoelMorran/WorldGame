3e2bf5dcf70a7682b58e2499be5b9350f76e4066
joel <jamorran@latrobe.students.edu.au>
2018-05-28 17:30:20 +1000

    test3

--- app.js
+++ app.js
@@ -6,10 +6,12 @@ const path = require('path');
 
 const app = express();
 
+app.use(express.static('dist'));
+
 app.unsubscribe(express.static(path.join(__dirname, 'dist')));
 
 app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, '../../dist/index.html'));
+    res.sendFile('index.html');
 });
 
 const port = process.env.PORT || '3001';
